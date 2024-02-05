800b58e6d713ada021a9af1378447b9e803f6f90
ummefahad <rokhaiyasultana97@gmail.com>
2024-02-05 14:53:20 +0530

    mlflow

---
+++ machine.py
@@ -0,0 +1,82 @@
+import os
+import warnings
+import sys
+import pandas as pd
+import numpy as np
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import ElasticNet
+from urllib.parse import urlparse
+import mlflow
+from mlflow.models.signature import infer_signature
+import mlflow.sklearn
+import logging
+from sklearn import datasets 
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+logging.basicConfig(level=logging.WARN)
+logger = logging.getLogger(__name__)
+
+def eval_metrics(actual,pred):
+    rmse = np.sqrt(mean_squared_error(actual, pred))
+    mae = mean_absolute_error(actual, pred)
+    r2 = r2_score(actual, pred)
+    return rmse, mae, r2
+X, y = datasets.load_iris(return_X_y=True)
+
+if __name__ == '__main__':
+    warnings.filterwarnings("ignore")
+    np.random.seed(40)
+
+    csv_url = "https://raw.githubusercontent.com/mlflow/mlflow/master/tests/datasets/winequality-red.csv"
+
+    try:
+        data = pd.read_csv(csv_url, sep=";")
+        train, test = train_test_split(data)
+
+        train_x = train.drop(["quality"], axis=1)
+        test_x = test.drop(['quality'], axis=1)
+        train_y = train[["quality"]]
+        test_y = test[['quality']]
+    except Exception as e:
+        logger.exception("Unable to download training & test CSV, check your internet connection. Error: %s", e)
+        sys.exit(1)
+
+alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
+l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
+
+with mlflow.start_run():
+        lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)  # Fix typo in class name
+        lr.fit(train_x, train_y)
+
+        predicted_qualities = lr.predict(test_x)  # Fix variable name here
+
+        (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+
+        print("Elasticnet model (alpha={:f}, l1_ratio={:f}):".format(alpha, l1_ratio))
+
+        print(" RMSE: %s" % rmse)
+        print(" MAE: %s" % mae)
+        print(" R2: %s" % r2)
+
+        mlflow.log_param("alpha", alpha)
+        mlflow.log_param("l1_ratio", l1_ratio)
+        mlflow.log_param("rmse", rmse)  # Fix variable name here
+        mlflow.log_param("mae", mae)  # Fix variable name here
+        mlflow.log_param("r2", r2)  # Fix variable name here
+
+        predictions = lr.predict(train_x)
+
+        signature = infer_signature(train_x, predictions)
+
+        tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+
+        if tracking_url_type_store != "file":
+            mlflow.sklearn.log_model(
+                lr, "model", registered_model_name="ElasticnetWineModel", signature=signature
+            )
+        else:
+            mlflow.sklearn.log_model(lr, "model", signature=signature)
+   
+
+   
